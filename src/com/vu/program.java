cba1efae1a0e3aa8b2fbeffd3a3aa2d84c411df2
erwanbrgs <bourgeois-e@acr-m4630g-304.tec.saint-louis.local>
2015-10-15 09:32:07 +0200

    ajout d'une ligne

--- src/com/vu/program.java
+++ src/com/vu/program.java
@@ -5,6 +5,7 @@ public class program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("essai1 github");
+		System.out.println("essai2 github");
 
 	}
 
